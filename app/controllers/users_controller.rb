14df782e27c8aee6f686f95f6e0431ad3827c133
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-20 23:37:06 -0700

    display multiple json responses in one page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,9 @@ class UsersController < ApplicationController
   # GET /users/1
   def show
     @user = User.find(params[:id])
-    render json: @user
+    @medications = Medication.all
+    response = {:user => @user, :medications => @medications}
+    render json: response
   end
 
   # POST /users
